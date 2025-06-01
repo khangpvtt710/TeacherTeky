535cbbef45b3efb870e6376966c0184ee357c658
khangpvtt710 <144519592+khangpvtt710@users.noreply.github.com>
2025-06-01 09:56:51 +0700

    1-6-2025
    1-6-2025

---
+++ Roblox/Doi_Mau_Khoi.lua
@@ -0,0 +1,10 @@
+while true do
+	game.Workspace.Part.BrickColor = BrickColor.new("Really red")
+	wait(1)
+	game.Workspace.Part.BrickColor = BrickColor.new("Black")
+	wait(1)
+	game.Workspace.Part.Transparency = 1
+	wait(1)
+	game.Workspace.Part.Transparency = 0
+	wait(1)
+end
