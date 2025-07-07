3313a021df829dcca57df5847c85a79c84c6a13c
khangpvtt710 <144519592+khangpvtt710@users.noreply.github.com>
2025-07-07 12:14:04 +0700

    7/7/2025
    7/7/2025

--- Python/HP1/BIEN_PRINT.py
+++ Python/HP1/BIEN_PRINT.py
@@ -5,3 +5,18 @@
 # |trường
 # |năm sinh:
 # -------------------------------------------
+
+a = int(input("nhập số: " ))
+print(a + 3)
+
+if ( a == 5):
+    a = a*a
+    print(a)
+elif(a>5):
+    print('YES')
+else:
+    print('NO')
+
+# viết ctrinh kiểm tra BMI khi nhập chiều cao và cân nặng
+# biết công thức tính BMI là cân nặng / (chiều cao)^2
+# sau khi nhập chiều cao cân năng thì trả về kết quả thiếu cân, thừa cân, ....
