efafc5cebc693bd8ff5e9e2d843911e8b9cc0cff
khangpvtt710 <144519592+khangpvtt710@users.noreply.github.com>
2025-07-09 13:23:39 +0700

    9/7/2025
    9/7/2025

---
+++ Python/HP1/WHILE_FOR.py
@@ -0,0 +1,17 @@
+a = 6
+
+while (a > 0):
+    if(a<2):
+        break 
+        continue
+    print(a)
+    a -= 1 
+
+for x in ['khang', 'tiến']:
+    print(x)
+for x in range(10):
+    print(x)
+for x in range(2,10):
+    print(x)
+for x in range(2,-10,-1):
+    print(x)
