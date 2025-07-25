d8394c8e3fdeb489fa24124bf2fa7dfac0438f51
khangpvtt710 <144519592+khangpvtt710@users.noreply.github.com>
2025-07-25 10:17:13 +0700

    25/7/2025
    25/7/2025

--- C++/Nhap_xuat.cpp
+++ C++/Nhap_xuat.cpp
@@ -4,7 +4,7 @@ int main()
 {
     int a; // string(#include string) , bool, float, double, long
     cout << "nhập a: ";
-    cin >> a;
+    cin >> a; // cin >> a >> b ;
     cout << "biến a là :" << a;
 
     return 0;
