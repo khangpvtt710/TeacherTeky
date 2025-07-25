7b2369384e5b3c0a854ccd56ec524f25f035a603
khangpvtt710 <144519592+khangpvtt710@users.noreply.github.com>
2025-07-25 09:26:05 +0700

    25/7/2025
    25/7/2025

---
+++ C++/Nhap_xuat.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std; // std::
+int main()
+{
+    int a; // string(#include string) , bool, float, double, long
+    cout << "nhập a: ";
+    cin >> a;
+    cout << "biến a là :" << a;
+
+    return 0;
+}
