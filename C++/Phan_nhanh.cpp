99da66ef4511fa3b62f2bc4128fa9e81d23a1a99
khangpvtt710 <144519592+khangpvtt710@users.noreply.github.com>
2025-07-28 08:11:50 +0700

    28/7/2025
    28/7/2025

---
+++ C++/Phan_nhanh.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a = 1;
+    int b = 10;
+    if (a > 5)
+    {
+        cout << a;
+    }
+    else
+    {
+        cout << "a nhỏ hơn 5" << endl;
+    }
+    if (b > 5)
+    {
+        cout << "b lớn hơn 5\n";
+        if (b == 10)
+        {
+            cout << "b=10";
+        }
+        else
+        {
+        }
+    }
+    else
+    {
+    }
+    return 0;
